ed017b581a6a046ba398c18bbabf26d8a5038fc3
Matheus-Matta <matheuseduardo3004@gmail.com>
2025-07-15 13:41:01 -0300

    prod

--- tmsapp/urls.py
+++ tmsapp/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.generic import TemplateView
-from .views import *
 
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
